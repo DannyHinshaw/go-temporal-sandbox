db88a6ab312e1e12dba4a65fc203ae9918ff03e1
Danny Hinshaw <danny.hinshaw@openly.com>
2021-08-06 09:46:00 -0400

    Fix health endpoint name.

--- pkg/api/handlers/health.go
+++ pkg/api/handlers/health.go
@@ -6,8 +6,8 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// GETHealth health check endpoint for API QA.
-func (h *handler) GETHealth(c echo.Context) error {
+// health endpoint to make sure API is healthy and functional.
+func (h *handler) health(c echo.Context) error {
 	// Should probably do some sort of Temporal health check here:
 	// https://community.temporal.io/t/temporal-client-worker-health-check/205/9
 	return c.JSON(http.StatusOK, "ok")
